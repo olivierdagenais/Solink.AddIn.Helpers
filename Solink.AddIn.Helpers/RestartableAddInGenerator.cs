da7aad34512d66ce53161f79a484884f7f5b61f4
Olivier Dagenais <olivier.dagenais@gmail.com>
2015-04-17 15:16:17 -0400

    TDD: CreateClass()

--- Solink.AddIn.Helpers/RestartableAddInGenerator.cs
+++ Solink.AddIn.Helpers/RestartableAddInGenerator.cs
@@ -1,9 +1,12 @@
 ﻿using System.CodeDom;
+using System.Reflection;
 
 namespace Solink.AddIn.Helpers
 {
     public class RestartableAddInGenerator
     {
+        private static readonly string RestartableAddInTypeName = typeof (RestartableAddIn<>).FullName;
+
         internal static CodeCompileUnit CreateCompileUnit()
         {
             var result = new CodeCompileUnit();
@@ -14,6 +17,23 @@ namespace Solink.AddIn.Helpers
         {
             var result = new CodeNamespace(namespaceName);
             compileUnit.Namespaces.Add(result);
+            return result;
+        }
+
+        internal static CodeTypeDeclaration CreateClass(CodeNamespace @namespace, string className, string hostViewFullName)
+        {
+            var result = new CodeTypeDeclaration(className)
+            {
+                IsClass = true,
+                TypeAttributes = TypeAttributes.Public | TypeAttributes.Sealed,
+            };
+            var hostViewType = new CodeTypeReference(hostViewFullName);
+            // subclasses "RestartableAddIn<IThing>"
+            result.BaseTypes.Add(new CodeTypeReference(RestartableAddInTypeName, hostViewType));
+            // implements "IThing"
+            result.BaseTypes.Add(hostViewType);
+
+            @namespace.Types.Add(result);
             return result;
         }
     }
