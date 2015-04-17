2a4546f768a6c63d1d50b91164bb2ce09d8f5bcd
Olivier Dagenais <olivier.dagenais@gmail.com>
2015-04-17 15:04:39 -0400

    TDD: CreateCompileUnit()

--- Solink.AddIn.Helpers/RestartableAddInGenerator.cs
+++ Solink.AddIn.Helpers/RestartableAddInGenerator.cs
@@ -1,6 +1,13 @@
-﻿namespace Solink.AddIn.Helpers
+﻿using System.CodeDom;
+
+namespace Solink.AddIn.Helpers
 {
     public class RestartableAddInGenerator
     {
+        internal static CodeCompileUnit CreateCompileUnit()
+        {
+            var result = new CodeCompileUnit();
+            return result;
+        }
     }
 }
